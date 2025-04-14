feded59022a12787592d9c30d4eaed751b6c8785
Ricardo Camacho <ing.camacho29@gmail.com>
2025-04-14 02:27:59 -0600

    Se agregan estilos de hover al componente itineraryCard

--- src/components/itineraryCard.jsx
+++ src/components/itineraryCard.jsx
@@ -13,14 +13,21 @@ export default function ItineraryCard({ itinerary }) {
   };
 
   return (
-    <div className="w-[300px] lg:w-[350px] bg-white rounded-xl shadow-md overflow-hidden border">
+    <div
+      className="w-[300px] lg:w-[350px] bg-white rounded-xl shadow-md overflow-hidden border 
+      transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-[0_20px_50px_rgba(202,138,4,0.5)]"
+    >
       {/* Header */}
       <div className="flex items-center p-4">
-        <img
-          className="h-10 w-10 rounded-full object-cover"
-          src={itinerary.userPhoto}
-          alt={`${itinerary.user} avatar`}
-        />
+        <div className="relative group">
+          <img
+            className="h-10 w-10 rounded-full object-cover transition-all duration-300
+        group-hover:scale-140 group-hover:ring-4 group-hover:ring-black/30 group-hover:ring-offset-2"
+            src={itinerary.userPhoto}
+            alt={`${itinerary.user} avatar`}
+          />
+          
+        </div>
         <div className="ml-3">
           <p className="text-sm font-semibold">{itinerary.user}</p>
           <p className="text-xs text-gray-500">{itinerary.city.name}</p>
@@ -37,16 +44,20 @@ export default function ItineraryCard({ itinerary }) {
       {/* Contenido principal */}
       <div className="p-4 text-sm text-gray-700 space-y-2">
         <div className="flex justify-between relative">
-        <button
+          <button
             onClick={handleLike}
             className={`
                 text-xl transition-all duration-300
                 hover:text-red-500 hover:scale-125
-                ${itinerary.likes > 0 ? 'text-red-500 scale-125' : 'text-gray-400'}
+                ${
+                  itinerary.likes > 0
+                    ? "text-red-500 scale-125"
+                    : "text-gray-400"
+                }
             `}
-            >
+          >
             <FaHeart />
-        </button>
+          </button>
           {itinerary.likes > 0 && (
             <span className="absolute -top-2 -left-2 text-sm text-gray-500 bg-white px-1 rounded-full shadow-sm">
               {itinerary.likes}
